fb69b4ee06daf554c5d54d1b73f1e9bc94e80171
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-03-23 00:39:46 +0330

    Write simple correct code

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import requests
+from pytube import YouTube
+
+
+def Download(lnk):
+    youtubeObject = YouTube(lnk)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+    print("Download is completed successfully")
+
+
+def check_url(tubeurl):
+    pattern = '"playabilityStatus":{"status":"ERROR","reason":"Video unavailable"'
+    request = requests.get(tubeurl, timeout=20)
+    return False if pattern in request.text else True
+
 
 if __name__ == "__main__":
-    print("OK")
+    link = "https://www.youtube.com/watch?v=OFrMD1iCCNQ"
+    if check_url(link):
+        Download(link)
